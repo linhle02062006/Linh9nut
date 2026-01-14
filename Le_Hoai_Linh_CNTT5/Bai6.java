daccab38b3e5231b212816453804912425d0081e
linhle02062006 <1350080137@sv.hcmunre.edu.vn>
2026-01-14 13:15:27 +0700

    Update Bai6.java

--- Le_Hoai_Linh_CNTT5/Bai6.java
+++ Le_Hoai_Linh_CNTT5/Bai6.java
@@ -7,16 +7,12 @@ public class Bai6 {
             a[i] = sc.nextInt();
         }
     }
-
-    // b. Xuất mảng
     public static void xuatMang(int[] a, int n) {
         for (int i = 0; i < n; i++) {
             System.out.print(a[i] + " ");
         }
         System.out.println();
     }
-
-    // c. Tìm vị trí số x
     public static int timViTriX(int[] a, int n, int x) {
         for (int i = 0; i < n; i++) {
             if (a[i] == x) {
@@ -25,8 +21,6 @@ public class Bai6 {
         }
         return -1;
     }
-
-    // d. Tìm giá trị lớn nhất
     public static int timMax(int[] a, int n) {
         int max = a[0];
         for (int i = 1; i < n; i++) {
@@ -36,8 +30,6 @@ public class Bai6 {
         }
         return max;
     }
-
-    // e. Tìm giá trị nhỏ nhất
     public static int timMin(int[] a, int n) {
         int min = a[0];
         for (int i = 1; i < n; i++) {
@@ -47,8 +39,6 @@ public class Bai6 {
         }
         return min;
     }
-
-    // f. Tìm vị trí phần tử lớn nhất
     public static int viTriMax(int[] a, int n) {
         int max = a[0];
         int vt = 0;
@@ -60,8 +50,6 @@ public class Bai6 {
         }
         return vt;
     }
-
-    // g. Sắp xếp mảng tăng dần
     public static void sapXepTangDan(int[] a, int n) {
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -73,8 +61,6 @@ public class Bai6 {
             }
         }
     }
-
-    // ================== HÀM MAIN ==================
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -82,15 +68,9 @@ public class Bai6 {
         int n = sc.nextInt();
 
         int[] a = new int[n];
-
-        // a
         nhapMang(a, n, sc);
-
-        // b
         System.out.println("Mang vua nhap:");
         xuatMang(a, n);
-
-        // c
         System.out.print("Nhap x can tim: ");
         int x = sc.nextInt();
         int vt = timViTriX(a, n, x);
@@ -99,19 +79,12 @@ public class Bai6 {
         } else {
             System.out.println("Khong tim thay x trong mang");
         }
-
-        // d
         System.out.println("Gia tri lon nhat trong mang: " + timMax(a, n));
-
-        // e
         System.out.println("Gia tri nho nhat trong mang: " + timMin(a, n));
-
-        // f
         System.out.println("Vi tri phan tu lon nhat: " + viTriMax(a, n));
-
-        // g
         sapXepTangDan(a, n);
         System.out.println("Mang sau khi sap xep tang dan:");
         xuatMang(a, n);
     }
 }
+
